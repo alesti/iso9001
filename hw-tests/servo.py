c0e9c1ef058d2805f45933951ab0e60a2b1a69ca
Alexander Stielau <alexander.stielau@googlemail.com>
2019-07-27 20:36:17 +0200

    better structure

--- hw-tests/servo.py
+++ hw-tests/servo.py
@@ -6,11 +6,15 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16) # Type des Boards, hier 16 Kanäle
 
+# Nullstellung
+# Drehung um XX ° gegen den Uhrzeigersinn
 kit.servo[0].angle = 0
+time.sleep(1)
+kit.servo[0].angle = 50
 time.sleep(5)
-kit.servo[0].angle = 90
-kit.servo[4].angle = 0
-kit.servo[4].angle = 120
+kit.servo[0].angle = 0
+#kit.servo[4].angle = 0
+#kit.servo[4].angle = 120
 
  
 
